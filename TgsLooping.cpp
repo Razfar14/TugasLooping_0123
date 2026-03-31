2988f8dcb4abace06fe0e0035a52953c8421ccd3
Razfar14 <rajarangkuty14@gmail.com>
2026-03-31 15:50:48 +0700

    membuatMenu

--- TgsLooping.cpp
+++ TgsLooping.cpp
@@ -5,7 +5,14 @@ using namespace std;
 int n, pilihan;
 
 
-
+void tampilkanMenu() {
+    cout << "\n========== MENU UTAMA ==========" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "9. Keluar" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Masukkan pilihan Anda: ";
+}
 
 
 
